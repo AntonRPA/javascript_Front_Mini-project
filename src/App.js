fb6d3bc57f088e1d36bb933bf2e9b94d58c31579
AntonRPA <jobtakeranton@mail.ru>
2023-01-31 00:55:34 +0600

    currency-convertor init

--- src/App.js
+++ src/App.js
@@ -1,91 +1,98 @@
-import React, { useEffect, useState } from 'react';
-import { Collection } from './Collection';
+import { useEffect, useRef, useState } from 'react';
+import { Block } from './Block';
 import './index.scss';
 
-const cats = [
-  { name: 'Все' },
-  { name: 'Море' },
-  { name: 'Горы' },
-  { name: 'Архитектура' },
-  { name: 'Города' },
-];
-
 function App() {
-  const [categoryId, setCategoryId] = useState(0);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(true);
-  const [searchValue, setsearchValue] = useState('');
-  const [collection, setCollection] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+  // const [rates, setRates] = useState({});
+  const ratesRef = useRef({});
 
-  //Получаем данные от бэкэнда
   useEffect(() => {
-    setIsLoading(true); //фикс для перехода по категориям, чтобы показывалась загрузка
-
-    const category = categoryId !== 0 ? `category=${categoryId}` : '';
-
-    fetch(
-      `https://63d6c88394e769375bb7744e.mockapi.io/photo_collection?page=${page}&limit=3&${category}`
-    )
+    fetch('https://www.cbr-xml-daily.ru/latest.js')
       .then((res) => res.json())
       .then((json) => {
-        setCollection(json);
+        // setRates(json.rates);
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
       })
       .catch((err) => {
         console.warn(err);
-        alert('Ошибка получения данных');
-      })
-      .finally(() => {
-        setIsLoading(false);
+        alert('Не удалось получить информацию');
       });
-  }, [categoryId, page]);
+  }, []);
 
-  //Рендерим компонент App
+  //Вычесление валюты относительно левого поля
+  const onChangeFromPrice = (value) => {
+    const price = () => {
+      if (fromCurrency === 'RUB') {
+        return value;
+      }
+      return value / ratesRef.current[fromCurrency];
+    };
+
+    const result = () => {
+      if (toCurrency === 'RUB' && fromCurrency === 'RUB') {
+        return value;
+      }
+      if (toCurrency === 'RUB') {
+        return (value / ratesRef.current[fromCurrency]).toFixed(3);
+      }
+      return (price() * ratesRef.current[toCurrency]).toFixed(3);
+    };
+
+    setFromPrice(value);
+    setToPrice(result());
+  };
+
+  //Вычисление валюты относительно правого поля
+  const onChangeToPrice = (value) => {
+    console.log(value);
+    const price1 = () => {
+      if (toCurrency === 'RUB') {
+        return value;
+      }
+      return value / ratesRef.current[toCurrency];
+    };
+
+    const result = () => {
+      if (toCurrency === 'RUB' && fromCurrency === 'RUB') {
+        return value;
+      }
+      if (fromCurrency === 'RUB') {
+        return (value / ratesRef.current[toCurrency]).toFixed(3);
+      }
+      return (price1() * ratesRef.current[fromCurrency]).toFixed(3);
+    };
+
+    setFromPrice(result());
+    setToPrice(value);
+  };
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <h1>Моя коллекция фотографий</h1>
-      <div className="top">
-        <ul className="tags">
-          {cats.map((obj, i) => (
-            <li
-              onClick={() => setCategoryId(i)}
-              className={categoryId === i ? 'active' : ''}
-              key={obj.name}
-            >
-              {obj.name}
-            </li>
-          ))}
-        </ul>
-        <input
-          value={searchValue}
-          onChange={(event) => setsearchValue(event.target.value)}
-          className="search-input"
-          placeholder="Поиск по названию"
-        />
-      </div>
-      <div className="content">
-        {isLoading ? (
-          <h2>Идет загрузка...</h2>
-        ) : (
-          collection
-            .filter((obj) => {
-              return obj.name.toLowerCase().includes(searchValue.toLowerCase());
-            })
-            .map((obj, index) => (
-              <Collection key={index} name={obj.name} images={obj.photos} />
-            ))
-        )}
-      </div>
-      <ul className="pagination">
-        {[...Array(5)].map((_, i) => (
-          <li
-            key={i}
-            onClick={() => setPage(i + 1)}
-            className={page === i + 1 ? 'active' : ''}
-          >
-            {i + 1}
-          </li>
-        ))}
-      </ul>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
